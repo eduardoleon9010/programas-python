ea31f95ca38e18c86b875c9fa6d37d6405e48bc4
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2024-01-06 19:11:19 -0500

    Update ciclo_while.py

--- ciclo_while.py
+++ ciclo_while.py
@@ -1,30 +1,17 @@
-"""
-Este programa realiza las siguientes acciones:
-
-Obtiene la fecha actual utilizando la librería datetime y muestra el día actual.
-Solicita al usuario ingresar un número entero.
-Realiza un ciclo controlado por contador (while) desde 1 hasta el número ingresado 
-por el usuario, mostrando en cada iteración el valor del contador.
-Solicita al usuario adivinar un número entre 1 y 10, y controla un ciclo hasta que 
-el usuario adivine el número secreto predefinido (5). Muestra en cuántos intentos acertó.
-Solicita al usuario ingresar el nombre de una amistad, convierte el nombre a 
-mayúsculas y muestra cada caracter del nombre hasta que encuentra la letra "A", 
-momento en el que se detiene el ciclo.
-Finaliza el programa mostrando "Fin".
-Este programa interactúa con el usuario para realizar ciclos controlados 
-por un contador, por eventos y también muestra cómo utilizar la sentencia break para 
-interrumpir un ciclo.
-"""
-
 from datetime import date
-hoy = date.today()                #Fecha actual
-print("Hoy es el dia: ", hoy)
+
+# Obtener la fecha actual utilizando la librería datetime y mostrar el día actual
+hoy = date.today()
+print("Hoy es el día:", hoy)
 print()
 print("TALLER 6 CICLOS ITERATIVOS CON LA SENTENCIA WHILE")
 print()
-num_1=int(input("Digite el primer numero: "))
 
-print("***Ciclo controlados por contador")
+# Solicitar al usuario ingresar un número entero
+num_1 = int(input("Digite el primer número: "))
+
+# Ciclo controlado por contador (while) desde 1 hasta el número ingresado por el usuario
+print("***Ciclo controlado por contador")
 i = 1
 while i <= num_1:
     print(i)
@@ -32,23 +19,27 @@ while i <= num_1:
 print('Fin del ciclo')
 
 print()
+
+# Ciclo controlado por eventos: solicita al usuario adivinar un número secreto y muestra en cuántos intentos acertó
 print("***Ciclo controlado por eventos")
-i = 1
-numero1=5
-numero2= int(input('Digite un numero de 1 a 10: '))
-while numero2 != numero1:
-    i += 1
-    numero2 = int(input("Digite un numeros de 1 a 10: "))
-print("Acertaste en el intento No.", 1)
+intentos = 1
+numero_secreto = 5  # Número secreto predefinido
+numero_usuario = int(input('Adivine un número del 1 al 10: '))
+while numero_usuario != numero_secreto:
+    intentos += 1
+    numero_usuario = int(input("Intenta adivinar el número secreto: "))
+print(f"Adivinaste el número en el intento No. {intentos}")
 print('Fin de ciclo')
 
 print()
+
+# Ciclo con la sentencia break: solicita al usuario ingresar el nombre de una amistad y muestra cada carácter del nombre en mayúsculas
 print("***Ciclo abortado con la sentencia break")
-amistad=input("Digite nombre de una amistad: ")
-amistas=amistad.upper()
-for character in amistad:
+nombre_amistad = input("Ingrese el nombre de una amistad: ")
+nombre_amistad = nombre_amistad.upper()
+for character in nombre_amistad:
     print(character)
-    if character=="A":
+    if character == "A":
         break
 print("Fin del ciclo")
 
