2c13232d674b90d2f224736570fa1c619df3db9f
Ing. Eduardo León <66545075+eduardoleon9010@users.noreply.github.com>
2024-01-06 19:22:33 -0500

    Update uso_if.py

--- uso_if.py
+++ uso_if.py
@@ -1,20 +1,13 @@
-"""
-Este programa hace lo siguiente:
-
-Solicita al usuario que introduzca un número entero.
-Verifica si el número ingresado es negativo.
-Si es negativo, imprime un mensaje indicando que no es válido, guarda el valor original 
-en una variable y, asigna el valor 42 a la variable x y luego muestra un mensaje indicando 
-que se decidió usar 42 en lugar del número original y.
-Si no es negativo, continúa sin hacer ningún cambio en x.
-Calcula la raíz cuadrada del valor final de x utilizando la función math.sqrt() y muestra 
-el resultado junto con un mensaje que indica cuál es el número del que se está calculando 
-la raíz cuadrada.
-Este programa verifica si el número ingresado es negativo y, en caso afirmativo, 
-reemplaza ese número con 42 antes de calcular la raíz cuadrada.
-"""
-
 import math
+
+# Este programa realiza varias acciones:
+# Solicita al usuario que introduzca un número entero.
+# Verifica si el número ingresado es negativo.
+# Si es negativo, imprime un mensaje indicando que no es válido, guarda el valor original en una variable y,
+# asigna el valor 42 a la variable x y luego muestra un mensaje indicando que se decidió usar 42 en lugar del número original.
+# Si no es negativo, continúa sin hacer ningún cambio en x.
+# Calcula la raíz cuadrada del valor final de x utilizando la función math.sqrt() y muestra el resultado junto con
+# un mensaje que indica cuál es el número del que se está calculando la raíz cuadrada.
 
 x = int(input("Digite un numero: "))
 
